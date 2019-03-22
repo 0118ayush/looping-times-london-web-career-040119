ef0e01ca9899925cd30c5550620a2ca452743412
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-22 17:43:50 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -5,8 +5,9 @@ def using_times
 	7.times do 
 	  puts "Wingardium Leviosa"
 	  
-	  attempts_left = attempts_left - lo
-	 
+	  attempts_left = attempts_left - 1
+    
+    puts "We still have #{attempts_left} attempts left!"	 
 	 
 	
 	end 
